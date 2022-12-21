33baf4d283ae56629c0f2107daa7dc32af375ff9
Charles-Pritchard <116393516+Charles-Pritchard@users.noreply.github.com>
2022-12-21 14:15:04 +1100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+let hand = 0
+input.onButtonPressed(Button.A, function () {
+    hand = randint(1, 3)
+    if (hand == 1) {
+        basic.showLeds(`
+            . # # # .
+            # # # # #
+            # # # # #
+            # # # # #
+            . # # # .
+            `)
+    } else if (hand == 2) {
+        basic.showLeds(`
+            . # # # .
+            . # # # .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
